35bd56d0349f0b72454868d63335d09ce8ccfdfb
LeeDavidMason <leemason7@googlemail.com>
2025-10-02 11:44:14 +0100

    Initial Commit

---
+++ src/main/java/com/citi/cto/ctointerviewpublic/CtoInterviewPublicApplication.java
@@ -0,0 +1,13 @@
+package com.citi.cto.ctointerviewpublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtoInterviewPublicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtoInterviewPublicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/citi/cto/ctointerviewpublic/CtoInterviewPublicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citi.cto.ctointerviewpublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtoInterviewPublicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
